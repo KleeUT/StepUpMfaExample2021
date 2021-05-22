0e5a6d08a2ecd3b60950f1f07ce5080f254529be
kleeut <klee.ut@gmail.com>
2021-05-22 12:43:32 +1000

    :art: ran prettier on Auth0 Actions.

--- actions/mfaOnFirstLogin.js
+++ actions/mfaOnFirstLogin.js
@@ -1,7 +1,8 @@
 // Auth0 Action
 exports.onExecutePostLogin = async (event, api) => {
   const enabledMfa = event.user.app_metadata.guardian;
-  if(!enabledMfa){
+  if (!enabledMfa) {
+    console.log("MFA not enrolled. Enrolling user.");
     // Require the user to do an MFA.
     api.multifactor.enable("guardian");
     // Update the user's metadata to represent that they've enabled MFA.
--- actions/stepUpMfa.js
+++ actions/stepUpMfa.js
@@ -1,10 +1,11 @@
-function extractScopesFromEvent(event){
+function extractScopesFromEvent(event) {
   return event.transaction ? event.transaction.requested_scopes || "" : "";
 }
- 
+
 exports.onExecutePostLogin = async (event, api) => {
   const scopes = extractScopesFromEvent(event);
-  if(scopes.includes("mfa:required")){
+  if (scopes.includes("mfa:required")) {
+    console.log("Requiring MFA");
     // Force the user to do a Guardian MFA.
     api.multifactor.enable("guardian");
     // Set to custom claim for when the MFA is complete.
